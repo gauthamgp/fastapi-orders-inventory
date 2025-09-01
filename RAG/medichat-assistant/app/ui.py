631fd27d8e7b79285652ca10dc4bb85034ef308d
P Gautham Pai <36203907+gauthamgp@users.noreply.github.com>
2025-09-01 11:13:17 +0530

    Delete RAG/medichat-assistant directory

--- RAG/medichat-assistant/app/ui.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-
-def pdf_uploader():
-    return st.file_uploader("Upload a PDF file", type=["pdf"],accept_multiple_files=True,
-                            help="Upload one or more PDF files to extract text and create a knowledge base.")
-
